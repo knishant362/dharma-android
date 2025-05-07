a02ede077144edd884a7932b8064739b8c2936a2
nishant.kumar <knishant362@gmail.com>
2025-05-07 22:41:45 +0530

    config: setup onesignal id

--- app/src/main/java/com/aurora/app/utils/Constants.kt
+++ app/src/main/java/com/aurora/app/utils/Constants.kt
@@ -2,10 +2,8 @@ package com.aurora.app.utils
 
 object Constants {
 
-    const val ONESIGNAL_APP_ID = ""
-
+    const val ONESIGNAL_APP_ID = "255ccccf-752e-43f4-afda-b7bbebf97f03"
     const val PACK_NAME = "LST"
-
     const val CONTENT_FILE = "data/main.json"
     const val IMAGE_DIRECTORY = "data/images"
 
