3c49dfec0692fc25369a71f1daf6a9c1124237cd
nishant.kumar <knishant362@gmail.com>
2025-09-16 23:24:15 +0530

    config: update build version v0.2(2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.sanatan.daily.mandir"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 2
+        versionName = "0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
